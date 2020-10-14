ab17a0d1337219d16aa5b752379a9e4fb388c7a4
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2020-10-14 14:13:18 +0300

    Added the "No Bullet Scale Limit" mod

---
+++ NoBulletScaleLimit/NoBulletScaleLimitModule.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace NoBulletScaleLimit
+{
+    public class NoBulletScaleLimitModule : ETGModule
+    {
+        public override void Init()
+        {
+        }
+
+        public override void Start()
+        {
+            ETGModMainBehaviour.Instance.gameObject.AddComponent<NoBulletScaleLimitBehaviour>();
+            Projectile.s_maxProjectileScale = float.MaxValue;
+            ETGModConsole.Log("No Bullet Scale Limit initialized.");
+        }
+
+        public override void Exit()
+        {
+        }
+    }
+
+    public class NoBulletScaleLimitBehaviour : MonoBehaviour
+    {
+        public void Update()
+        {
+            if(Projectile.s_maxProjectileScale != float.MaxValue)
+            {
+                Projectile.s_maxProjectileScale = float.MaxValue;
+            }
+        }
+    }
+}
